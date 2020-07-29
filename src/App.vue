a108f1ed03322f2b25a25532ce711339e1e46a5b
qijiao <1003297709@qq.com>
2020-07-29 11:59:23 +0800

    重置布局

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <div class="group_heads">
+      <span>制作答题卡</span>
+    </div>
+    <router-view />
+  </div>
 </template>
 
 <style lang="less">
-#app {
-  background: red;
+@import '~@/assets/css/variables.less';
+.group_heads {
+  height: 50px;
+  line-height: 50px;
+  width: 100%;
+  background-color: @main;
+  position: fixed;
+  z-index: 10;
+  text-align: center;
+  color: @white;
+  font-size: 16px;
+  top: 0;
 }
 </style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,39 +1,11 @@
 <template>
-  <div>
+  <div class="page-content">
     s
   </div>
 </template>
 
-<script>
-import { mapState, mapActions } from 'vuex'
-export default {
-  name: 'app',
-  components: {},
-  data() {
-    return {}
-  },
-  computed: {
-    ...mapState('answerSheet', ['GroupDataArr', 'PageLayout']),
-  },
-  mounted() {
-    const TestData = [
-      {
-        id: 1,
-        height: 380,
-        questionType: 'AnswerSheetTitle',
-        content: [{ title: '' }],
-      },
-      { id: 2, height: 120, questionType: 'ObjectiveQuestion', content: [] },
-      // { id: 3, height: 90, questionType: '', content: [] },
-      // { id: 4, height: 900, questionType: '', content: [] },
-      // { id: 5, height: 350, questionType: '', content: [] }
-    ]
-    this.groupPage(TestData)
-  },
-  methods: {
-    ...mapActions('answerSheet', ['editGroupData', 'groupPage', 'editLayout']),
-  },
+<style lang="less" scoped>
+.page-content {
+  margin-top: 50px;
 }
-</script>
-
-<style lang="scss" scoped></style>
+</style>
