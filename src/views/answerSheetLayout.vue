0f8330303ab415c13e557422db354c3719a67d5a
qijiao <1003297709@qq.com>
2020-07-29 13:52:24 +0800

    新增答题卡布局

---
+++ src/views/answerSheetLayout.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="Guide_bar">
+    <div class="allscore">
+      当前总分：
+      <span>0</span> 分
+    </div>
+    <div class="layout-box">
+      <div class="layout-top">
+        <span>布局</span>
+        <span class="layout-edit">修改</span>
+      </div>
+      <div class="layout-bottom">
+        <div>A3/B4/8K纸</div>
+        <div>两栏</div>
+        <div>线上阅卷</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+@import '~@/assets/css/variables.less';
+.Guide_bar {
+  position: fixed;
+  left: 7px;
+  z-index: 2;
+  margin-top: 40px;
+  .allscore {
+    font-size: 14px;
+  }
+  .layout-box {
+    border: 1px solid @main;
+    min-width: 93px;
+    background: @white;
+    margin-top: 10px;
+    font-size: 14px;
+  }
+  .layout-top {
+    background-color: @font-f7f8;
+    padding: 5px 8px;
+  }
+  .layout-top {
+    background-color: @font-f7f8;
+    padding: 5px 8px;
+  }
+  .layout-edit {
+    color: @mainFont !important;
+    cursor: pointer;
+    float: right;
+  }
+  .layout-bottom {
+    background-color: @white;
+    padding: 10px 8px;
+    line-height: 22px;
+  }
+}
+</style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,11 +1,28 @@
 <template>
   <div class="page-content">
-    s
+    <answer-sheet-layout />
+    <div class="main-info"></div>
   </div>
 </template>
 
+<script>
+import answerSheetLayout from './answerSheetLayout'
+export default {
+  components: {
+    answerSheetLayout,
+  },
+}
+</script>
 <style lang="less" scoped>
 .page-content {
   margin-top: 50px;
+  position: relative;
+}
+.main-info {
+  height: 100%;
+  padding-top: 50px;
+  position: absolute;
+  top: 0;
+  width: 100%;
 }
 </style>
