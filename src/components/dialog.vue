830415ca3f7d18e865451c9f24920e402e37378c
qijiao <1003297709@qq.com>
2020-07-29 16:18:56 +0800

    弹出框组件

---
+++ src/components/dialog.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="hj-dialog">
+    <el-dialog
+      v-if="dialogDrag"
+      v-bind="$attrs"
+      v-on="$listeners"
+      :title="title"
+      :close-on-click-modal="closeOnClickModal"
+      v-dialog-drag
+    >
+      <slot></slot>
+    </el-dialog>
+    <el-dialog
+      v-else
+      v-bind="$attrs"
+      v-on="$listeners"
+      :title="title"
+      :close-on-click-modal="closeOnClickModal"
+    >
+      <slot></slot>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import { Dialog } from 'element-ui'
+export default {
+  name: 'hjDialog',
+  props: {
+    title: { type: String },
+    closeOnClickModal: {
+      type: Boolean,
+      default: false,
+    },
+    dialogDrag: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  data() {
+    return {}
+  },
+  methods: {},
+  mounted() {},
+  components: {
+    elDialog: Dialog,
+  },
+}
+</script>
+
+<style>
+.el-dialog__header {
+  text-align: center;
+  padding-bottom: 0;
+}
+.el-dialog__headerbtn {
+  top: 12px;
+  right: 12px;
+}
+.el-dialog__title {
+  color: #333;
+}
+.el-dialog__headerbtn .el-dialog__close {
+  /* color: white; */
+  font-size: 23px;
+}
+.public-dialog .el-button {
+  padding: 8px 15px;
+}
+.el-dialog__body {
+  text-align: left;
+  padding: 10px 20px 15px;
+}
+
+.dialog-footer {
+  text-align: right;
+  padding-top: 20px;
+}
+.dialog-footer > div {
+  display: inline-block;
+}
+.dialog-footer > div:nth-of-type(1) {
+  margin-right: 15px;
+}
+</style>
