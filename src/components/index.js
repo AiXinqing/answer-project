ff90590e1fb8beb5c7892b6992029cda88432179
qijiao <1003297709@qq.com>
2020-07-29 16:56:18 +0800

    按钮组件

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,7 @@ import {
   Message
 } from 'element-ui'
 import HjDialog from './dialog'
+import HjButton from './button'
 
 export default {
   install: (Vue) => {
@@ -15,6 +16,7 @@ export default {
     Vue.prototype.$prompt = MessageBox.prompt;
     // Vue.prototype.$notify = Notification;    //IE兼容
     Vue.component('hj-dialog', HjDialog)
+    Vue.component('hj-button', HjButton)
 
   }
 }
