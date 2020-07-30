086f4baae3dd44b1d77c5cc52df315a4a2bd4609
qijiao <1003297709@qq.com>
2020-07-30 13:53:07 +0800

    标题store

---
+++ src/store/modules/answerSheetTitle.js
@@ -0,0 +1,48 @@
+const state = {
+  precautions: {
+    // 答题卡标题信息
+    textarea: '',
+    studentInfo: [{
+        // 学生信息
+        name: '考号',
+        checked: true,
+      },
+      {
+        name: '学校',
+        checked: false,
+      },
+      {
+        name: '姓名',
+        checked: false,
+      },
+      {
+        name: '班级',
+        checked: true,
+      },
+      {
+        name: '学号',
+        checked: false,
+      },
+    ],
+    AdmissionTicket: 8, // 准考证号
+  },
+}
+
+const mutations = {
+  setPrecautionsText(state, val) {
+    state.precautions.textarea = val
+  },
+}
+
+const actions = {
+  PrecautionsTextFunc: (context, layoutVal) => {
+    context.commit('setPrecautionsText', layoutVal)
+  },
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions,
+}
