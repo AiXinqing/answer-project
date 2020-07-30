d6c3870afe9eb6462575d81e5653e657d2a929ce
qijiao <1003297709@qq.com>
2020-07-30 13:54:18 +0800

    答题卡标题组件

---
+++ src/views/questionContent/Precautions/_textarea.vue
@@ -0,0 +1,35 @@
+<template>
+  <el-input
+    type="textarea"
+    :rows="2"
+    placeholder="请输入答题卡标题"
+    v-model="textareaVal"
+    @input="editPrecautionsTitile"
+    resize="none"
+  />
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+export default {
+  props: {
+    textareaData: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      textareaVal: this.textareaData,
+    }
+  },
+  methods: {
+    ...mapActions('answerSheetTitle', ['PrecautionsTextFunc']),
+    editPrecautionsTitile(e) {
+      this.PrecautionsTextFunc(e)
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
