6ddd11bc82ce4ab316c4ffdea43625fbfcdb1e65
qijiao <1003297709@qq.com>
2020-07-30 14:08:47 +0800

    学生标题栏显示组件

---
+++ src/views/questionContent/Precautions/_studentInfo.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-row class="TestTitle">
+    <el-col
+      v-for="(item, i) in checkedInfo"
+      :key="i"
+      :span="4"
+      class="title-item"
+    >
+      <span>{{ item.name }}</span>
+      <span v-if="i == checkedInfo.length - 1"
+        ><span class="titke-edit">编辑</span></span
+      >
+      <span v-else />
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  props: {
+    studentData: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  computed: {
+    checkedInfo() {
+      return this.studentData.filter((item) => item.checked)
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
