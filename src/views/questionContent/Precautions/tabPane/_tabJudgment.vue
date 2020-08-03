9fe6b80bb2489598e89b3b9def764ae1d3345fc3
qijiao <1003297709@qq.com>
2020-08-03 18:37:34 +0800

    判断小题详情

--- src/views/questionContent/Precautions/tabPane/_tabJudgment.vue
+++ src/views/questionContent/Precautions/tabPane/_tabJudgment.vue
@@ -63,10 +63,35 @@ export default {
     },
     singleBoxHanlde () {
       // 新增题组-小题详情
-      if (this.tabStatus) {
-        console.log(this.tabStatusVal)
-      } else {
-        console.log(this.tabStatusVal)
+      const StatusObj = {
+        val: this.tabStatusVal,
+        status: this.tabStatus
+      }
+      this.$emit('hanlde-status', StatusObj)
+      if (!this.tabStatus) {
+        let subtopicArr = []
+        for (let index = this.itemStart; index <= this.itemEnd; index++) {
+          let subtopic = {
+            pid: this.itemData.id,
+            id: 'judgment_' + index,
+            score: parseFloat(this.itemScore),
+            select: parseInt(this.itemSelect),
+            topic: index
+          }
+          subtopicArr.push(subtopic)
+        }
+        let itemObj = {
+          type: 'judgment',
+          data: {
+            start: parseInt(this.itemStart),
+            end: parseInt(this.itemEnd),
+            score: parseFloat(this.itemScore),
+            select: parseInt(this.itemSelect),
+            id: this.itemData.id,
+            childGroup: subtopicArr
+          }
+        }
+        this.$emit('hanlde-add-group-question', itemObj)
       }
     }
   },
