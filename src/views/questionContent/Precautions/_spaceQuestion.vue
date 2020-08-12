e850e34cd2000aad3108858ea5738bed4f4dec83
AiXinqing <1003297709@qq.com>
2020-08-12 10:48:44 +0800

    新增

---
+++ src/views/questionContent/Precautions/_spaceQuestion.vue
@@ -0,0 +1,23 @@
+<template>
+<!-- 填空题题组 -->
+  <div class="space_box">
+    <div>1</div>
+    <div class="add_question" @click="hanldeAddSubtopic">+ 分段添加小题</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      hanldeAddSubtopic() {
+
+      }
+    },
+  }
+</script>
+
+<style lang="less">
+  .space_box{
+    margin-top: 20px;
+  }
+</style>
