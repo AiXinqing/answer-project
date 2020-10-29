d58d956db553fff5d8103f518d2c29551cbe464a
qijiao <1003297709@qq.com>
2020-10-29 10:55:18 +0800

    删除无用文件

--- src/views/Subassembly/fillInTheBlank/index.vue
+++ src/views/Subassembly/fillInTheBlank/index.vue
@@ -134,4 +134,11 @@
   .add_question {
     margin-top: 20px;
   }
+  .question-group {
+    width: 100%;
+    height: 200px;
+    overflow: auto;
+    border: 1px solid #888;
+    margin-top: 15px;
+  }
 </style>
