a58d548e1c7fecb60fd67b7a41750375e6329d37
qijiao <1003297709@qq.com>
2021-01-07 14:53:03 +0800

    富文本编辑

--- src/components/tinymce/index.vue
+++ src/components/tinymce/index.vue
@@ -13,7 +13,6 @@
   import './plugins/code/plugin.min.js'
   import './plugins/mathjax/plugin.min.js'
   import './plugins/mathjax/config.js'
-  // import './plugins/'
 
   export default {
     props: {
@@ -44,18 +43,10 @@
         this.editor.remove();
     },
 
-    watch: {
-        // 外部修改 v-model 绑定的 html 的值时更新编辑器的内容
-        // value(newValue, oldValue) {
-        //     if (newValue != this.editor.getContent()) {
-        //         this.editor.setContent(newValue || oldValue);
-        //     }
-        // }
-    },
     mounted() {
       this.initEditor()
-
     },
+
     methods: {
       initEditor(){
         const self = this
@@ -68,13 +59,14 @@
           language: 'zh_CN',
           menubar: false,
           paste_data_images: true,
+
           setup: function(editor) {
               // 创建工具栏按钮
               // 添加上传图片按钮
               editor.ui.registry.addButton('image', { icon: 'image', title: '插入图片', onclick: () => {
-                  // this.uploadMode = 0;
                   console.log('上传测试')
               } });
+
               // 编辑器内容发生变化后更新 html 的内容
               editor.on('blur', (e) => {
                   let obj = {
@@ -84,10 +76,10 @@
                   self.$emit('input', obj)
               })
           },
-          images_upload_handler: function (blobInfo, success, failure){
-            let formData = new FormData()
-            console.log(blobInfo.filename())
 
+          images_upload_handler: function (blobInfo, success, failure){
+
+            let formData = new FormData()
             formData.append('img',blobInfo.blob())
             self.$axios.post('http://127.0.0.1:8000/upload/',formData)
                 .then(response =>{
@@ -99,7 +91,6 @@
                         failure('上传失败！')
                     }
                 })
-
           }
 
         }).then(editors => {
@@ -113,80 +104,80 @@
 </script>
 
 <style lang="less">
-@import url('./skins/ui/oxide/skin.min.css');
-@import url('./skins/ui/oxide/content.min.css');
-@import url('./skins/ui/oxide/content.inline.min.css');
+  @import url('./skins/ui/oxide/skin.min.css');
+  @import url('./skins/ui/oxide/content.min.css');
+  @import url('./skins/ui/oxide/content.inline.min.css');
 
-.tox {
-  .tox-tbtn--bespoke{
-    .tox-tbtn__select-label{
-      width: 4em;
+  .tox {
+    .tox-tbtn--bespoke{
+      .tox-tbtn__select-label{
+        width: 4em;
+      }
+    }
+    .tox-tbtn{
+      height: 30px;
+    }
+    .tox-toolbar__group{
+      height: 30px;
+      border-bottom: 1px solid #ddd;
     }
   }
-  .tox-tbtn{
-    height: 30px;
+  .rich-text {
+      // 微调 Full featured 的编辑器阴影, 工具栏阴影
+      .mce-tinymce {
+          box-shadow: 0 0px 1px rgba(0, 0, 0, 0.25);
+
+          .mce-top-part::before {
+              box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
+          }
+      }
+
+      // inline 模式的边框和得到焦点的样式
+      .mce-content-body {
+          padding: 8px;
+          border: 1px solid #DDD;
+          border-radius: 3px;
+
+          &.mce-edit-focus {
+              outline: none;
+              border-color: #47a4f5;
+              box-shadow: 2px 2px 3px rgba(0, 0, 0, 0.1) inset;
+          }
+      }
+
+      .mce-notification {
+          display: none;
+      }
   }
-  .tox-toolbar__group{
-    height: 30px;
-    border-bottom: 1px solid #ddd;
+
+  // 工具栏样式: 缩小字体, 修改按钮的 hover 背景色等
+  .mce-panel {
+      border: none !important;
+
+      .mce-toolbar .mce-btn-group {
+          padding: 0;
+
+          &:not(:first-child) {
+              margin-left: 0;
+          }
+
+          .mce-btn {
+              margin-left: 0;
+
+              &:hover {
+                  border-color: transparent;
+                  background: #808695;
+                  color: white;
+
+                  .mce-ico, button {
+                      color: white;
+                  }
+              }
+
+              .mce-ico {
+                  font-size: 14px;
+              }
+          }
+      }
   }
-}
-.rich-text {
-    // 微调 Full featured 的编辑器阴影, 工具栏阴影
-    .mce-tinymce {
-        box-shadow: 0 0px 1px rgba(0, 0, 0, 0.25);
-
-        .mce-top-part::before {
-            box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
-        }
-    }
-
-    // inline 模式的边框和得到焦点的样式
-    .mce-content-body {
-        padding: 8px;
-        border: 1px solid #DDD;
-        border-radius: 3px;
-
-        &.mce-edit-focus {
-            outline: none;
-            border-color: #47a4f5;
-            box-shadow: 2px 2px 3px rgba(0, 0, 0, 0.1) inset;
-        }
-    }
-
-    .mce-notification {
-        display: none;
-    }
-}
-
-// 工具栏样式: 缩小字体, 修改按钮的 hover 背景色等
-.mce-panel {
-    border: none !important;
-
-    .mce-toolbar .mce-btn-group {
-        padding: 0;
-
-        &:not(:first-child) {
-            margin-left: 0;
-        }
-
-        .mce-btn {
-            margin-left: 0;
-
-            &:hover {
-                border-color: transparent;
-                background: #808695;
-                color: white;
-
-                .mce-ico, button {
-                    color: white;
-                }
-            }
-
-            .mce-ico {
-                font-size: 14px;
-            }
-        }
-    }
-}
 </style>
--- src/components/tinymce/triggerEditor/index.vue
+++ src/components/tinymce/triggerEditor/index.vue
@@ -64,7 +64,6 @@
         const self = this
         tinymce.init({
           selector:`#tinymce_${this.editorId}`,
-          // auto_focus: true,
           inline:true,
           toolbar: this.toolbar,
           fontsize_formats: '11px 12px 14px 16px 18px 24px 36px 48px',
@@ -75,13 +74,14 @@
           language: 'zh_CN',
           menubar: false,
           paste_data_images: true,
+
           setup: function(editor) {
               // 创建工具栏按钮
               // 添加上传图片按钮
               editor.ui.registry.addButton('image', { icon: 'image', title: '插入图片', onclick: () => {
-                  // this.uploadMode = 0;
                   console.log('上传测试')
               } });
+
               // 编辑器内容发生变化后更新 html 的内容
               editor.on('blur', (e) => {
                   let obj = {
@@ -109,9 +109,9 @@
                 }
               });
           },
+
           images_upload_handler: function (blobInfo, success, failure){
             let formData = new FormData()
-            console.log(blobInfo.filename())
 
             formData.append('img',blobInfo.blob())
             self.$axios.post('http://127.0.0.1:8000/upload/',formData)
@@ -124,7 +124,6 @@
                         failure('上传失败！')
                     }
                 })
-
           }
 
         }).then(editors => {
@@ -138,80 +137,80 @@
 </script>
 
 <style lang="less">
-@import url('../skins/ui/oxide/skin.min.css');
-@import url('../skins/ui/oxide/content.min.css');
-@import url('../skins/ui/oxide/content.inline.min.css');
+  @import url('../skins/ui/oxide/skin.min.css');
+  @import url('../skins/ui/oxide/content.min.css');
+  @import url('../skins/ui/oxide/content.inline.min.css');
 
-.tox {
-  .tox-tbtn--bespoke{
-    .tox-tbtn__select-label{
-      width: 4em;
+  .tox {
+    .tox-tbtn--bespoke{
+      .tox-tbtn__select-label{
+        width: 4em;
+      }
+    }
+    .tox-tbtn{
+      height: 30px;
+    }
+    .tox-toolbar__group{
+      height: 30px;
+      border-bottom: 1px solid #ddd;
     }
   }
-  .tox-tbtn{
-    height: 30px;
+  .rich-text {
+      // 微调 Full featured 的编辑器阴影, 工具栏阴影
+      .mce-tinymce {
+          box-shadow: 0 0px 1px rgba(0, 0, 0, 0.25);
+
+          .mce-top-part::before {
+              box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
+          }
+      }
+
+      // inline 模式的边框和得到焦点的样式
+      .mce-content-body {
+          padding: 8px;
+          border: 1px solid #DDD;
+          border-radius: 3px;
+
+          &.mce-edit-focus {
+              outline: none;
+              border-color: #47a4f5;
+              box-shadow: 2px 2px 3px rgba(0, 0, 0, 0.1) inset;
+          }
+      }
+
+      .mce-notification {
+          display: none;
+      }
   }
-  .tox-toolbar__group{
-    height: 30px;
-    border-bottom: 1px solid #ddd;
+
+  // 工具栏样式: 缩小字体, 修改按钮的 hover 背景色等
+  .mce-panel {
+      border: none !important;
+
+      .mce-toolbar .mce-btn-group {
+          padding: 0;
+
+          &:not(:first-child) {
+              margin-left: 0;
+          }
+
+          .mce-btn {
+              margin-left: 0;
+
+              &:hover {
+                  border-color: transparent;
+                  background: #808695;
+                  color: white;
+
+                  .mce-ico, button {
+                      color: white;
+                  }
+              }
+
+              .mce-ico {
+                  font-size: 14px;
+              }
+          }
+      }
   }
-}
-.rich-text {
-    // 微调 Full featured 的编辑器阴影, 工具栏阴影
-    .mce-tinymce {
-        box-shadow: 0 0px 1px rgba(0, 0, 0, 0.25);
-
-        .mce-top-part::before {
-            box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
-        }
-    }
-
-    // inline 模式的边框和得到焦点的样式
-    .mce-content-body {
-        padding: 8px;
-        border: 1px solid #DDD;
-        border-radius: 3px;
-
-        &.mce-edit-focus {
-            outline: none;
-            border-color: #47a4f5;
-            box-shadow: 2px 2px 3px rgba(0, 0, 0, 0.1) inset;
-        }
-    }
-
-    .mce-notification {
-        display: none;
-    }
-}
-
-// 工具栏样式: 缩小字体, 修改按钮的 hover 背景色等
-.mce-panel {
-    border: none !important;
-
-    .mce-toolbar .mce-btn-group {
-        padding: 0;
-
-        &:not(:first-child) {
-            margin-left: 0;
-        }
-
-        .mce-btn {
-            margin-left: 0;
-
-            &:hover {
-                border-color: transparent;
-                background: #808695;
-                color: white;
-
-                .mce-ico, button {
-                    color: white;
-                }
-            }
-
-            .mce-ico {
-                font-size: 14px;
-            }
-        }
-    }
-}
 </style>
