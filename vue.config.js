c819707a999ff7b2cf9fff401248f9655497fdf0
qijiao <1003297709@qq.com>
2021-03-02 10:11:41 +0800

    跨域配置

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,16 @@ module.exports = {
 
   devServer: {
     port: 8080,
-    open: true
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://192.168.0.56:8012',     //设置你调用的接口域名和端口号 别忘了加http
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '',     //这里理解成用'/api'代替target里面的地址，后面组件中我们掉接口时直接用api          代替 比如我要调用'http://xxx.xxx.xxx.xx:8081/user/add'，直接写‘/api/user/add’即可
+        }
+      }
+    }
   },
 
   css: {
@@ -12,5 +21,6 @@ module.exports = {
         data: '@import "@/scss/settings.scss";'
       }
     }
-  }
+  },
+
 }
