2631f6ae2f064650e452b4e79dd49fdf9574cb85
qijiao <1003297709@qq.com>
2021-03-16 14:01:58 +0800

    更新服务器地址

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 export const URL = {
   AXIOS_TIMEOUT:10000,
-  SERVICE_CONTEXT_PATH:'http://192.168.0.56:8012', // http://localhost:60044 http://192.168.0.56:8012
+  SERVICE_CONTEXT_PATH:'http://localhost:60044', // http://localhost:60044 http://192.168.0.56:8012
   SERVICE_UPLOAD_PICTURE:'http://192.168.0.56:8011' // 图片上传服务器端口
 }
