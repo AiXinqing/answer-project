3e5c995e44e662766a2a7a1da6afbbda1888e46f
qijiao <1003297709@qq.com>
2021-03-25 16:48:22 +0800

    服务器地址

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 export const URL = {
   AXIOS_TIMEOUT:10000,
   SERVICE_CONTEXT_PATH:'/', // http://localhost:60044 http://192.168.0.56:8012
-  SERVICE_UPLOAD_PICTURE:'http://lzxxt.cn:60041/' // 图片上传服务器端口 //http://172.16.10.99:60041/
+  SERVICE_UPLOAD_PICTURE:'https://lzxxt.cn:60041/' // 图片上传服务器端口 //http://172.16.10.99:60041/
 }
