71f5a89ab93a8e6e5cb2214f66d1947a24364091
qijiao <1003297709@qq.com>
2021-03-26 14:40:35 +0800

    新增阅卷-路由

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,12 @@ const routes = [{
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import( /* webpackChunkName: "about" */ '@/views/previewContent.vue')
+  },
+  {
+    // 阅卷
+    path: '/exam',
+    name: 'preview',
+    component:() => import('@/views/exam/')
   }
 ]
 
