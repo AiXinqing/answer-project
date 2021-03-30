7a904821f18b8af8dd6d4f5caf4457a86658125c
qijiao <1003297709@qq.com>
2021-03-30 10:58:35 +0800

    接口api

---
+++ src/config/api.js
@@ -0,0 +1,12 @@
+export const API_VERSION = 'v1'
+
+let BASEURL = 'http://localhost:60044'
+
+if(process.env.NODE_ENV === 'development'){
+    BASEURL = 'http://192.168.0.54:8094'
+}
+
+export const URL = {
+  setCookies: BASEURL + '/api/services/common/user/refreshtoken', //cookies获取覆盖
+  setLogin:BASEURL +'/api/services/common/user/login',
+}
