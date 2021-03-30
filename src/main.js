f45f05c67ee9b2235952a6535a82ae4da8f24fcd
qijiao <1003297709@qq.com>
2021-03-30 11:35:35 +0800

    引入插件

--- src/main.js
+++ src/main.js
@@ -3,18 +3,25 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/element-up.js'
-import Element from 'element-ui'
+import { Element,Message }from 'element-ui'
 import Components from './components/index'
 import httpRequest from './utils/request'
+import {post, fetch, patch, put, postForm} from './utils/http'
 import lodash from 'lodash'
 
 Vue.prototype.$http = httpRequest
 Vue.prototype.$lodash = lodash
 Vue.config.productionTip = false
+Vue.prototype.$post = post
+Vue.prototype.$fetch = fetch
+Vue.prototype.$patch = patch
+Vue.prototype.$put = put
+Vue.prototype.$postForm = postForm
+Vue.prototype.$message = Message
+
+
 Vue.use(Components)
 Vue.use(httpRequest)
-
-
 Vue.use(Element, {
   size: 'medium'
 })
