17d11f5574da3a6a415c4930fe2e1259a8d6fa64
qijiao <1003297709@qq.com>
2021-03-31 15:37:15 +0800

    添加动态组件

---
+++ src/views/exam/examContent/complexReport/gradebook/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="complex_content">
+    123
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="less">
+  @import '~@/assets/css/variables.less';
+  .complex_content{
+    width: 100%;
+    background-color: @white;
+    margin-top: 10px;
+  }
+</style>
