c37e7bfab31101ff04b1a614b7c9f0c196114a46
qijiao <1003297709@qq.com>
2021-04-01 15:25:49 +0800

    新增组件

---
+++ src/components/elementUi/examButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <el-button v-bind="$attrs" v-on="$listeners">
+    <slot></slot>
+  </el-button>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="less">
+  @import '~@/assets/css/variables.less';
+</style>
