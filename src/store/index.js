a4e09f26eed4a2f5296a52303bcfd97c9ca64a9b
qijiao <1003297709@qq.com>
2021-05-10 17:26:13 +0800

    班级分析报告 布局

--- src/store/index.js
+++ src/store/index.js
@@ -13,7 +13,7 @@ const modulesFiles = require.context('./modules', true, /\.js$/)
 const modules = modulesFiles.keys().reduce((modules, modulePath) => {
   // set './app.js' => 'app'
   const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1')
-  console.log(moduleName)
+  // console.log(moduleName)
   const value = modulesFiles(modulePath)
   modules[moduleName] = value.default
   return modules
--- src/views/exam/examContent/classAnalysis/index.vue
+++ src/views/exam/examContent/classAnalysis/index.vue
@@ -1,26 +1,82 @@
 <template>
-  <div class="classAnalysis_content">
-    <!-- 标题 -->
-    <div class="classAnalysis_title mar_T10">
-      <div class="title_h2">20年秋期九年级半期考试</div>
-      <div class="title_h5">班级对比分析报告</div>
+  <section>
+    <div class="classAnalysis_content">
+      <!-- 标题 -->
+      <div class="classAnalysis_title mar_T10">
+        <div class="title_h2">20年秋期九年级半期考试</div>
+        <div class="title_h5">班级对比分析报告</div>
+      </div>
+      <div class="title_info">
+        <div class="title_p padding_10">老师您好：</div>
+        <div class="title_p">本次考试（考试时间：2020-11-09)， 全校 初三 共 24 个班级，1334 名学生参加考试。 考试学科： 语文、数学、英语、物理化学(物理、化学)、物理、化学、生物、道法历史(道法、历史)、历史、地理、道法 11 门学科。</div>
+      </div>
+      <!-- 标题 -->
+
+      <!-- 动态锚点 -->
+      <keep-alive>
+      <!--  缓存路由的name属性等于tabView的组件 -->
+        <component v-bind:is="tabView" class="mar_T10"></component>
+      </keep-alive>
+      <!-- 动态锚点 -->
     </div>
-    <div class="title_info">
-      <div class="title_p padding_10">老师您好：</div>
-      <div class="title_p">本次考试（考试时间：2020-11-09)， 全校 初三 共 24 个班级，1334 名学生参加考试。 考试学科： 语文、数学、英语、物理化学(物理、化学)、物理、化学、生物、道法历史(道法、历史)、历史、地理、道法 11 门学科。</div>
+
+    <!-- 浮动锚点点击处 -->
+    <div class="Anchor_box">
+      <div class="anchor_title">成绩分析</div>
+      <div class="anchor_list">
+        <p
+          v-for="(tab ,index) in tabsAnchor"
+          :key="index"
+          :class="{cur:iscur==index}"
+          @click="iscur=index,tabChange('Parking' + (index + 1))"
+        >
+          {{tab.name}}
+        </p>
+      </div>
     </div>
-    <!-- 标题 -->
-  </div>
+    <!-- 浮动锚点点击处 -->
+
+  </section>
 </template>
 
 <script>
+  import Parking1 from './_averageRank'
+  import Parking2 from './_gradesOverview'
+
   export default {
-    methods: {
-      goAnchor(selector) {
-          var anchor = this.$el.querySelector(selector) // 参数为要跳转到的元素id
-          document.body.scrollTop = anchor.offsetTop; // chrome
-          document.documentElement.scrollTop = anchor.offsetTop; // firefox
+    components: {
+      Parking1,
+      Parking2
+    },
+    data() {
+      return {
+        tabView: 'Parking1',
+        tabsAnchor: [
+          {
+            name:'学科平均分排名'
+          },
+          {
+            name:'班级成绩概况'
+          },
+          {
+            name:'学科等级占比'
+          },
+          {
+            name:'分段人数分布'
+          },
+          {
+            name:'学科试题得分率'
+          },
+        ],
+        iscur:0
       }
+    },
+    methods: {
+
+      tabChange (tab) {
+        this.tabView = tab
+      }
+
     }
   }
 </script>
@@ -77,6 +133,39 @@
 
     .padding_10{
       padding-bottom: 15px;
+    }
+
+  }
+  .Anchor_box{
+    width: 180px;
+    position: fixed;
+    left: 100px;
+    top: 35%;
+    z-index: 999;
+    background-color: @white;
+
+    .anchor_title{
+      height: 52px;
+      line-height: 52px;
+      background-color: @main_t;
+      font-weight: 700;
+      text-align: center;
+    }
+
+    .anchor_list{
+      padding: 10px 0 20px 0;
+      width: 100%;
+      background-color: @white;
+
+      p{
+        width: 100%;
+        text-align: center;
+        cursor: pointer;
+
+        &.cur{
+          color: @main
+        }
+      }
     }
   }
 </style>
