c6310114ef8b10ab71fab11e769672b3beab570f
qijiao <1003297709@qq.com>
2021-05-12 16:08:15 +0800

    拆分store下文件

--- src/store/index.js
+++ src/store/index.js
@@ -12,8 +12,9 @@ const modulesFiles = require.context('./modules', true, /\.js$/)
 // it will auto require all vuex module from modules file
 const modules = modulesFiles.keys().reduce((modules, modulePath) => {
   // set './app.js' => 'app'
-  const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1')
-  // console.log(moduleName)
+  // const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1')
+  const moduleName = modulePath.replace(/(.*\/)*([^.]+).*/ig, "$2")
+
   const value = modulesFiles(modulePath)
   modules[moduleName] = value.default
   return modules
