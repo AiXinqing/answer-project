85f23d9f35aa3642080ab16a97173eea856954f0
qijiao <1003297709@qq.com>
2021-05-12 14:04:46 +0800

    测试服务器下载地址

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,7 @@
 export const URL = {
   AXIOS_TIMEOUT:10000,
   SERVICE_CONTEXT_PATH:'/', // http://localhost:60044 http://192.168.0.56:8012
-  SERVICE_UPLOAD_PICTURE:'https://lzxxt.cn:60041/' // 图片上传服务器端口 //http://172.16.10.99:60041/
+  SERVICE_UPLOAD_PICTURE:'http://localhost:63993/' //'https://lzxxt.cn:60041/' // 图片上传服务器端口 //http://172.16.10.99:60041/
 }
 
 export function getCookie(name){
