e4057d51611c714674e405ee1fe01484ea3dd2a7
qijiao <1003297709@qq.com>
2021-05-20 16:47:02 +0800

    教师分析-首页 - 考试

--- src/views/teacherAnalysis/analysisContent/index.vue
+++ src/views/teacherAnalysis/analysisContent/index.vue
@@ -1,15 +1,180 @@
 <template>
-  <div>
+  <div class="teacher_wapper">
+    <div class="exam_subject mr_10">
 
+      <div class="select_box">
+        <div class="search_select">
+          <div class="select_label">切换考试：</div>
+          <hj-select
+            :items="examOptions"
+            size="mini"
+            :value="exam"
+            @change="handelExamChange"
+          ></hj-select>
+        </div>
+        <div class="search_select">
+          <div class="select_label">切换班级：</div>
+          <hj-select
+            :items="classOptions"
+            size="mini"
+            :value="classSelect"
+            @change="handelClassChange"
+          ></hj-select>
+        </div>
+      </div>
+
+      <div class="subject_box">
+        <div class="subject_left">选择科目：</div>
+        <div class="subject_right">
+
+          <div
+            :class="['subject_item',{'active':item.id == subjectSelect}]"
+            v-for="item in subjectBox"
+            :key="item.id"
+            @click="subjectChange(item)"
+          >{{item.name}}</div>
+
+        </div>
+      </div>
+    </div>
+
+    <overall-overview class="mr_10" />
   </div>
 </template>
 
 <script>
+  import overallOverview from "./teacherHome/overallOverview"
   export default {
-    
+
+    components: {
+      overallOverview,
+    },
+
+    data() {
+      return {
+        examOptions: [
+          {
+            label:'2020年秋期八年级入学考试',
+            value:0,
+          }
+        ],
+        classOptions:[
+          {
+            label:'2班',
+            value:0,
+          }
+        ],
+        exam:0,
+        classSelect:0,
+        subjectBox:[
+          {
+            name:'全部',
+            id:0
+          },
+          {
+            name:'语文',
+            id:1
+          },
+          {
+            name:'数学',
+            id:2
+          }
+        ],
+        subjectSelect:0
+      }
+    },
+
+    methods: {
+      handelExamChange() {
+
+      },
+      handelClassChange() {
+
+      },
+
+      subjectChange(item){
+        this.subjectSelect = item.id
+      }
+    },
   }
 </script>
 
 <style lang="less">
+  @import '~@/assets/css/variables.less';
+
+  .teacher_wapper{
+    width: 1200px;
+    position: relative;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  .exam_subject{
+    padding: 20px 0;
+    background-color: @white;
+    width: 100%;
+
+    &>div{
+      margin: 0 10px;
+    }
+
+    .select_box{
+      display: flex;
+      width: clac(100% - 20px);
+
+      .search_select{
+        display: flex;
+        width: 50%;
+
+        .select_label{
+          font-size: 14px;
+          line-height: 28px;
+        }
+
+        .hj-select{
+          width: 250px;
+          .el-select{
+            width: 250px;
+          }
+        }
+
+      }
+    }
+
+    .subject_box{
+      display: flex;
+      font-size: 14px;
+
+      .subject_left{
+        line-height: 28px;
+        margin-top: 20px;
+        width: 75px;
+      }
+    }
+  }
+
+  .mr_10{
+    margin-top: 10px;
+  }
+
+  .subject_right{
+    display: flex;
+    width: 100%;
+
+    .subject_item{
+      padding: 0 15px;
+      border-radius: 4px;
+      height: 28px;
+      line-height: 28px;
+      cursor: pointer;
+      margin-left: 10px;
+      margin-top: 20px;
+
+      &.active{
+        background-color: @main;
+        color: @white;
+      }
+    }
+  }
 
 </style>
---
+++ src/views/teacherAnalysis/analysisContent/teacherHome/overallOverview/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="overall_overview">
+    <div class="title_name">
+      <span>整体概况</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="less">
+  @import '~@/assets/css/variables.less';
+  .overall_overview{
+    background-color: @white;
+    padding:20px 0;
+    width: 100%;
+
+    &>div{
+      margin:0 10px;
+    }
+  }
+</style>
