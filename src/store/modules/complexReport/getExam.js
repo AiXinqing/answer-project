783c525d200ec6de396a2e7c49e7bada3dcac5a6
qijiao <1003297709@qq.com>
2021-05-31 16:36:55 +0800

    获取数据loading

--- src/store/modules/complexReport/getExam.js
+++ src/store/modules/complexReport/getExam.js
@@ -36,7 +36,6 @@
       }))
       //考试信息
       state.examData = data
-
     },
 
     SET_HEADERTABLE: (state, res) => {
@@ -47,10 +46,16 @@
     SET_TABLE: (state, res) => {
       const { data } = res.ResponseContent
       state.TableList = data
+      state.tableLoading = false
     },
 
     GET_PAGE: (state,page) => {
       state.pagination = page
+    },
+
+    // 设置loading
+    SET_LOADING_TRUE: (state) => {
+      state.tableLoading = true
     }
   }
 
@@ -65,7 +70,9 @@
     GetStuResults({ commit }, padata) {
       return new Promise((resolve, reject) => {
         const { tid, tsid, cids, keyWords, pageIndex, pageSize, url } = padata
-        GetStuResults({ tid, tsid, cids, keyWords, pageIndex, pageSize,url}).then(res => {
+
+        GetStuResults({ tid, tsid, cids, keyWords, pageIndex, pageSize, url }).then(res => {
+          commit('SET_LOADING_TRUE')
           commit('SET_TABLE', res)
           commit('GET_PAGE', {
             pageSize: res.ResponseContent.pageSize,
