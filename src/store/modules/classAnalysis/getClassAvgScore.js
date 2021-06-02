af951e3d9695bbc295f6d75f6a2d8efdf48d7cae
qijiao <1003297709@qq.com>
2021-06-02 16:50:46 +0800

    班级 loading false

--- src/store/modules/classAnalysis/getClassAvgScore.js
+++ src/store/modules/classAnalysis/getClassAvgScore.js
@@ -16,6 +16,10 @@
 
     SET_LOADING: (state) => {
       state.tableLoading = true
+    },
+
+    SET_LOADING_FALSE: (state) => {
+      state.tableLoading = false
     }
 
   }
@@ -31,6 +35,7 @@
           resolve(res)
           return res
         }).catch(error => {
+          commit('SET_LOADING_FALSE')
           reject(error)
         })
       })
