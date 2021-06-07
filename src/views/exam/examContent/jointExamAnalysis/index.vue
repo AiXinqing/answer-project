7a136cc93805359b0377da822a149e0a1a91f747
qijiao <1003297709@qq.com>
2021-06-07 14:37:49 +0800

    联考成绩册-布局

--- src/views/exam/examContent/jointExamAnalysis/index.vue
+++ src/views/exam/examContent/jointExamAnalysis/index.vue
@@ -1,15 +1,47 @@
 <template>
-  <div>
-
+  <div class="exam_wapper">
+    <hj-tabs
+      v-model="activeName"
+      :tab-pane="tabPaneBox"
+      @tab-click="handleClick"
+    >
+      <component
+        :is="activeName"
+        :exam-info="examInfo"
+        :prmTid="prmTid"
+        ref="tabName"
+      ></component>
+    </hj-tabs>
   </div>
 </template>
 
 <script>
+  import jointExam from './jointExam'
   export default {
+    components: {
+      jointExam,
+    },
 
+    data() {
+      return {
+        activeName: 'jointExam',
+        tabPaneBox:[
+          {
+            label:'联考成绩册',
+            name:'jointExam'
+          },
+        ]
+      }
+    },
+
+    methods: {
+      handleClick() {
+
+      }
+    },
   }
 </script>
 
-<style lang="scss" scoped>
+<style lang="less">
 
 </style>
